7bb72ec48cfd3f6798dc588f9a0614e53f46250b
Dodo <dodo@chaosdorf.de>
2014-06-15 20:50:22 +0200

    heavy KI

--- bots2.py
+++ bots2.py
@@ -97,6 +97,68 @@ def danger(field,agressor):
 	
 	return []
 
+def victory(field,ownChar):
+	result = field	
+	for i in range(3):
+		row = field[i]		
+		charCounter = 0
+		for place in row:
+			if place == ownChar:
+				charCounter += 1
+		if charCounter == 2:
+			for i2 in range(3):
+				if field[i][i2] == ' ':
+					result[i][i2] = ownChar
+					return result
+	columns = [[],[],[]]
+	for row in field:
+		columns[0].append(row[0])
+		columns[1].append(row[1])
+		columns[2].append(row[2])
+	for i in range(3):
+		col = columns[i]
+		charCounter = 0
+		for i2 in range(3):
+			if col[i2] == ownChar:
+				charCounter += 1
+		if charCounter == 2:
+			for i2 in range(3):
+				if field[i2][i] == ' ':
+					result[i2][i] = ownChar
+					return result
+	dia1 = [field[0][0],field[1][1],field[2][2]]
+	dia2 = [field[0][2],field[1][1],field[2][0]]
+	charCounter = 0
+	for i in range(3):
+		if dia1[i] == ownChar:
+			charCounter += 1
+	if charCounter == 2:
+		if dia1[0] == ' ':
+			result[0][0] = ownChar
+			return result
+		elif dia1[1] == ' ':
+			result[1][1] = ownChar
+			return result
+		elif dia1[2] == ' ':
+			result[2][2] = ownChar
+			return result
+
+	charCounter = 0
+	for i in range(3):
+		if dia2[i] == ownChar:
+			charCounter += 1
+	if charCounter == 2:
+		if dia2[0] == ' ':
+			result[0][2] = ownChar
+			return result
+		elif dia2[1] == ' ':
+			result[1][1] = ownChar
+			return result
+		elif dia2[2] == ' ':
+			result[2][0] = ownChar
+			return result
+	return []
+
 def KI1_Turn(field):
 	fieldStatus = isFieldFull(field)
 	if fieldStatus == True:
@@ -260,6 +322,92 @@ def KI3_Turn(field):
 	result[row][column] = 'O'
 	return result
 
+def KI4_Turn(field):
+	# are there any empty places?
+	fieldStatus = isFieldFull(field)
+	if fieldStatus == True:
+		return field
+
+	result = field
+	print "It is the turn of the AI2.0."
+
+	# Can i win?
+
+	vic = victory(field,'O')
+	if vic != []:
+		return vic
+	
+	# Am I at risk?
+
+	dan = danger(field,'X')
+	if dan == []:
+		pass
+	else:
+		return dan
+
+	# Which field has in itself?
+
+	for i in range(3):
+		if 'O' in field[i] and 'X' not in field[i]:
+			if field[i][0] == ' ':
+				result[i][0] = 'O'
+				return result
+			elif field[i][1] == ' ':
+				field[i][1] = 'O'
+				return result
+			elif result[i][2] == ' ':
+				result[i][2] = 'O'
+				return result
+
+	columns = [[],[],[]]
+	for row in field:
+		columns[0].append(row[0])
+		columns[1].append(row[1])
+		columns[2].append(row[2])
+	
+	for i in range(3):
+		col = columns[i]
+		if 'O' in col and 'X' not in col:
+			if field[0][i] == ' ':
+				result[0][i] = 'O'
+				return result
+			elif field[1][i] == ' ':
+				result[1][i] = 'O'
+				return result
+			elif field[2][i] == 'O':
+				result[2][i] = 'O'
+				return result
+
+	dia1 = [field[0][0],field[1][1],field[2][2]]
+	dia2 = [field[0][2],field[1][1],field[2][0]]
+
+	if 'O' in dia1 and 'X' not in dia1:
+		if dia1[0] == ' ':
+			result[0][0] = 'O'
+			return result
+		elif dia1[1] == ' ':
+			field[1][1] = 'O'
+			return result
+		elif dia1[2] == ' ':
+			result[2][2] = 'O'
+			return result
+
+	if 'O' in dia2 and 'X' not in dia2:
+		if dia2[0] == ' ':
+			result[0][2] = 'O'
+			return result
+		elif dia2[1] == ' ':
+			field[1][1] = 'O'
+			return result
+		elif dia2[2] == ' ':
+			result[2][0] = 'O'
+			return result
+
+	row = random.randint(0,2)
+	column = random.randint(0,2)
+	result[row][column] = 'O'
+
+	return result
 
 def USER_Turn(field):
 	fieldStatus = isFieldFull(field)
@@ -335,7 +483,7 @@ computer2Score = 0
 com1Wins = 0
 com2Wins = 0
 counter = 0
-number = 100
+number = 1000
 
 while counter != number:
 	#print counter
@@ -364,7 +512,7 @@ while counter != number:
 		
 		turn += 1
 		#print 'Turn: ' + str(turn)
-		field = KI3_Turn(field)
+		field = KI4_Turn(field)
 		#printField(field)		
 		win = Winner(field)
 		if win == 'Computer1':
--- tic2.py
+++ tic2.py
@@ -16,6 +16,7 @@ player2Score = 0
 print '0 : random KI'
 print '1 : easy KI'
 print '2 : medium KI'
+print '3 : heavy KI'
 print 'q : quit' 
 enemy = raw_input('Choose your enemy: ')
 if enemy == 'q' or enemy == 'Q':
@@ -119,6 +120,68 @@ def danger(field,agressor):
 	
 	return []
 
+def victory(field,ownChar):
+	result = field	
+	for i in range(3):
+		row = field[i]		
+		charCounter = 0
+		for place in row:
+			if place == ownChar:
+				charCounter += 1
+		if charCounter == 2:
+			for i2 in range(3):
+				if field[i][i2] == ' ':
+					result[i][i2] = ownChar
+					return result
+	columns = [[],[],[]]
+	for row in field:
+		columns[0].append(row[0])
+		columns[1].append(row[1])
+		columns[2].append(row[2])
+	for i in range(3):
+		col = columns[i]
+		charCounter = 0
+		for i2 in range(3):
+			if col[i2] == ownChar:
+				charCounter += 1
+		if charCounter == 2:
+			for i2 in range(3):
+				if field[i2][i] == ' ':
+					result[i2][i] = ownChar
+					return result
+	dia1 = [field[0][0],field[1][1],field[2][2]]
+	dia2 = [field[0][2],field[1][1],field[2][0]]
+	charCounter = 0
+	for i in range(3):
+		if dia1[i] == ownChar:
+			charCounter += 1
+	if charCounter == 2:
+		if dia1[0] == ' ':
+			result[0][0] = ownChar
+			return result
+		elif dia1[1] == ' ':
+			result[1][1] = ownChar
+			return result
+		elif dia1[2] == ' ':
+			result[2][2] = ownChar
+			return result
+
+	charCounter = 0
+	for i in range(3):
+		if dia2[i] == ownChar:
+			charCounter += 1
+	if charCounter == 2:
+		if dia2[0] == ' ':
+			result[0][2] = ownChar
+			return result
+		elif dia2[1] == ' ':
+			result[1][1] = ownChar
+			return result
+		elif dia2[2] == ' ':
+			result[2][0] = ownChar
+			return result
+	return []
+
 def KI1_Turn(field):
 	fieldStatus = isFieldFull(field)
 	if fieldStatus == True:
@@ -211,6 +274,93 @@ def KI3_Turn(field):
 
 	result = field
 	print "It is the turn of the AI2.0."
+	
+	# Am I at risk?
+
+	dan = danger(field,'X')
+	if dan == []:
+		pass
+	else:
+		return dan
+
+	# Which field has in itself?
+
+	for i in range(3):
+		if 'O' in field[i] and 'X' not in field[i]:
+			if field[i][0] == ' ':
+				result[i][0] = 'O'
+				return result
+			elif field[i][1] == ' ':
+				field[i][1] = 'O'
+				return result
+			elif result[i][2] == ' ':
+				result[i][2] = 'O'
+				return result
+
+	columns = [[],[],[]]
+	for row in field:
+		columns[0].append(row[0])
+		columns[1].append(row[1])
+		columns[2].append(row[2])
+	
+	for i in range(3):
+		col = columns[i]
+		if 'O' in col and 'X' not in col:
+			if field[0][i] == ' ':
+				result[0][i] = 'O'
+				return result
+			elif field[1][i] == ' ':
+				result[1][i] = 'O'
+				return result
+			elif field[2][i] == 'O':
+				result[2][i] = 'O'
+				return result
+
+	dia1 = [field[0][0],field[1][1],field[2][2]]
+	dia2 = [field[0][2],field[1][1],field[2][0]]
+
+	if 'O' in dia1 and 'X' not in dia1:
+		if dia1[0] == ' ':
+			result[0][0] = 'O'
+			return result
+		elif dia1[1] == ' ':
+			field[1][1] = 'O'
+			return result
+		elif dia1[2] == ' ':
+			result[2][2] = 'O'
+			return result
+
+	if 'O' in dia2 and 'X' not in dia2:
+		if dia2[0] == ' ':
+			result[0][2] = 'O'
+			return result
+		elif dia2[1] == ' ':
+			field[1][1] = 'O'
+			return result
+		elif dia2[2] == ' ':
+			result[2][0] = 'O'
+			return result
+
+	row = random.randint(0,2)
+	column = random.randint(0,2)
+	result[row][column] = 'O'
+
+	return result
+
+def KI4_Turn(field):
+	# are there any empty places?
+	fieldStatus = isFieldFull(field)
+	if fieldStatus == True:
+		return field
+
+	result = field
+	print "It is the turn of the AI2.0."
+
+	# Can i win?
+
+	vic = victory(field,'O')
+	if vic != []:
+		return vic
 	
 	# Am I at risk?
 
@@ -393,6 +543,8 @@ while answer != 'q':
 			field = KI2_Turn(field)
 		elif enemy == '2':
 			field = KI3_Turn(field)
+		elif enemy == '3':
+			field = KI4_Turn(field)
 
 		win = Winner(field)
 		if win == player2Name:
